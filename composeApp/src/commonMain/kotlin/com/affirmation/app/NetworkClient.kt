1f4a0bd595d70bfbe37c092c831a27caf285e75f
yurasika25 <yrasika80@gmail.com>
2025-12-31 13:09:04 +0000

    add HttpTimeout

--- composeApp/src/commonMain/kotlin/com/affirmation/app/NetworkClient.kt
+++ composeApp/src/commonMain/kotlin/com/affirmation/app/NetworkClient.kt
@@ -1,6 +1,7 @@
 package com.affirmation.app
 
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
@@ -11,6 +12,13 @@ expect fun platformHttpClient(): HttpClient
 
 fun createHttpClient(): HttpClient =
     platformHttpClient().config {
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 15_000
+            connectTimeoutMillis = 10_000
+            socketTimeoutMillis = 15_000
+        }
+
         install(ContentNegotiation) {
             json(
                 Json {
